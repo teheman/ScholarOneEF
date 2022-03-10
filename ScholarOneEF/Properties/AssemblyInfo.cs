43e5526fa92011c35fc0d137501de303317ad681
Tom Eheman <eheman@gmail.com>
2022-03-09 21:20:26 -0500

    update description

--- ScholarOneEF/Properties/AssemblyInfo.cs
+++ ScholarOneEF/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScholarOne")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library to consume ScholarOne API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ScholarOne")]
